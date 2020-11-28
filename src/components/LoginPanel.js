e33b5822aa0a97fa65f78e874e2027921a434a64
krzysztof <misiek.nalepa@wp.pl>
2020-11-28 07:11:10 +0100

    Basic login panel

---
+++ src/components/LoginPanel.js
@@ -0,0 +1,33 @@
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+
+
+const LoginPanel = ({ apiKey }) => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {console.log(data); axios.post('http://142.93.110.208/authenticate', data)
+        .then(response => console.log(response.data));}
+
+
+    return (
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group row">
+                <label htmlFor="inputUsername" className="col-sm-2 col-form-label">Username</label>
+                <div className="col-sm-12">
+                    <input type="username" name="username" className="form-control" id="inputUsername"  ref={register}/>
+                </div>
+            </div>
+            <div className="form-group row">
+                <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                <div className="col-sm-12">
+                    <input type="password" name="password" className="form-control" id="inputPassword" ref={register} />
+                </div>
+            </div>
+            <button type="submit" className="btn btn-primary">Zaloguj</button>
+
+        </form>
+    )
+}
+
+export default LoginPanel;
